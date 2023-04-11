45e28cc857fae6c6c1295870409135c668584df7
addwebcontent <shaygaming500@gmail.com>
2023-04-11 18:16:37 +0300

    first commit mobile first

---
+++ script.js
@@ -0,0 +1,13 @@
+function revealOnScroll() {
+    const boxes = document.querySelectorAll('.box');
+    const windowHeight = window.innerHeight;
+    boxes.forEach(box => {
+      const boxTop = box.getBoundingClientRect().top;
+      if (boxTop < windowHeight) {
+        box.classList.add('visible');
+      } else {
+        box.classList.remove('visible');
+      }
+    });
+  }
+
